d926e37490b8cbaa5aca4d926c9a462a16bc66e6
Ray <ray@dropee.com>
2019-01-17 17:00:33 +0700

    update code

--- mailer.js
+++ mailer.js
@@ -25,81 +25,64 @@ let mailList = [{
     attachments: 'https://drive.google.com/file/d/0BzdGvyU-5w7ic3RhcnRlcl9maWxlX2Rhc2hlclYw/view?usp=sharing'
 }]
 
-async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
-}
-
+// If attachment folder is not exist, create one
 if (!fs.existsSync(ABSPATH + '/attachments')) {
     fs.mkdirSync(ABSPATH + '/attachments');
-    console.log('Folder Created')
+    console.log('No folder found! Folder created')
 }
-
-const start = async () => {
-    asyncForEach(mailList, async (el, i) => {
-        // Load client secrets from a local file.
-        await fs.readFile('credentials.json', (err, content) => {
-            if (err) return console.log('Error loading client secret file:', err);
-            // Authorize a client with credentials, then call the Google Drive API.
-            authorize(JSON.parse(content), downloadFile);
-            console.log('FIle Downloaded')
-        });
-    
-        function downloadFile(auth) {
-            const drive = google.drive({
-                version: 'v3',
-                auth
-            });
-    
-            let fileId = el.attachments.match(/[-\w]{25,}/);
-            let dest = fs.createWriteStream(ABSPATH + '/attachments/document_' + i + '.pdf');
-    
-            drive.files.get({
-                fileId: fileId,
-                alt: 'media'
-            }, {
-                responseType: 'stream'
-            }, (err, res) => {
-                res.data
-                    .on('end', () => {
-                        console.log('Done');
-                    })
-                    .on('error', err => {
-                        console.log('Error', err);
-                    })
-                    .pipe(dest);
-            })
-        }
-    
-        await console.log('done')
-    
-        // const options = {
-        //     from: 'Ray',
-        //     to: el.mail,
-        //     cc: 'rayrinaldy@gmail.com',
-        //     subject: 'Hello From Dropee',
-        //     text: el.text,
-        //     attachments: [{
-        //         filename: 'Attachment.pdf',
-        //         content: fs.createReadStream(ABSPATH + '/attachments/document_' + i + '.pdf')
-        //     }]
-        // };
-    
-        // transporter.sendMail(options, (error, info) => {
-        //     if (error) {
-        //         console.log(error, 'XXXXXXX')
-        //     } else {
-        //         console.log(info)
-        //     }
-        // });
-    
-    })
-    
-    console.log('Done');
-}
-
-start();
-
 
 // Let the loop begin
+mailList.forEach((el, i) => {
+    // Load client secrets from a local file.
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Drive API.
+        authorize(JSON.parse(content), downloadFile);
+        console.log('File Downloaded')
+    });
+
+    function downloadFile(auth) {
+        const drive = google.drive({
+            version: 'v3',
+            auth
+        });
+
+        let fileId = el.attachments.match(/[-\w]{25,}/);
+        let dest = fs.createWriteStream(ABSPATH + '/attachments/document_' + i + '.pdf');
+
+        drive.files.get({
+            fileId: fileId,
+            alt: 'media'
+        }, {
+            responseType: 'stream'
+        }, (err, res) => {
+            res.data
+                .on('end', () => {
+                    const options = {
+                        from: 'Ray',
+                        to: el.mail,
+                        cc: 'rayrinaldy@gmail.com',
+                        subject: 'Hello From Dropee',
+                        text: el.text,
+                        attachments: [{
+                            filename: 'Attachment.pdf',
+                            content: fs.createReadStream(ABSPATH + '/attachments/document_' + i + '.pdf')
+                        }]
+                    };
+                    
+                    // Sending email
+                    transporter.sendMail(options, (error, info) => {
+                        if (error) {
+                            console.log(error, 'XXXXXXX')
+                        } else {
+                            console.log('Email Sent Successfully')
+                        }
+                    });
+                })
+                .on('error', err => {
+                    console.log('Error', err);
+                })
+                .pipe(dest);
+        })
+    }
+})
