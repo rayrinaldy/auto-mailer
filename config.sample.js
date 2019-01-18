7cd019594e0c65c460774f1c11a64478cfa56d0c
Ray <ray@dropee.com>
2019-01-18 15:08:21 +0700

    added csv support

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,14 @@
 module.exports = {
-    gmail: {
-        username: 'xxx@gmail.com',
-        password: 'xxx'
+    dropee: {
+        user: "xxxxxxxx", 
+        clientId: "xxxxxxx-xxxx.apps.googleusercontent.com",
+        clientSecret: "xxxxx-xxxx",
+        refreshToken: "1/xxx-xxxx",
+    },
+    hakama: {
+        user: "xxxx@gmail.com", 
+        clientId: "xxxx-xxx.apps.googleusercontent.com",
+        clientSecret: "xxxxx",
+        refreshToken: "1/xxxxxxxxx",
     }
 }
--- mailer.js
+++ mailer.js
@@ -3,96 +3,153 @@
 import fs from 'fs';
 import path from 'path';
 import nodemailer from 'nodemailer';
-import csvtojson from "csvtojson";
+import csv from "csvtojson";
 import ora from 'ora';
 import config from './config';
 import { google } from 'googleapis';
 import { authorize, getAccessToken } from './lib/drive';
-const ABSPATH = path.dirname(process.mainModule.filename); // Absolute path to our app directory
+
+// Absolute path to our app directory
+const ABSPATH = path.dirname(process.mainModule.filename); 
+
+const htmlTemplate = fs.createReadStream(ABSPATH + '/views/hakama_template.html',{encoding:'utf-8'});
 
 // Emails credentials
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.gmail.com',
+    service: 'gmail',
+    port: 465,
+    secure: true,
     auth: {
-        user: config.gmail.username,
-        pass: config.gmail.password
+        type: 'OAuth2',
+        user: config.dropee.user, 
+        clientId: config.dropee.clientId,
+        clientSecret: config.dropee.clientSecret,
+        refreshToken: config.dropee.refreshToken,
+        expires: 1484314697598
     }
 });
 
-// Gonna change this to CSV later
-let mailList = [{
-    mail: 'ray@dropee.com',
-    text: 'Test Automation Mail 123',
-    attachments: 'https://drive.google.com/file/d/0BzdGvyU-5w7ic3RhcnRlcl9maWxlX2Rhc2hlclYw/view?usp=sharing'
-}]
+const csvFilePath = './mail.csv';
 
-// If attachment folder is not exist, create one
-if (!fs.existsSync(ABSPATH + '/attachments')) {
-    const spinner = ora().start('No folder found!');
-    fs.mkdirSync(ABSPATH + '/attachments');
-    spinner.succeed('Folder created');
-}
+// // Gonna change this to CSV later
+// let mailList = [{
+//     mail: 'rayrinaldy@gmail.com',
+//     text: 'Test Automation Mail With Attachments',
+//     attachments: 'https://drive.google.com/file/d/0BzdGvyU-5w7ic3RhcnRlcl9maWxlX2Rhc2hlclYw/view?usp=sharing'
+// }]
 
-// Let the loop begin
-mailList.forEach((el, i) => {
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-        if (err) return console.log('Error loading client secret file:', err);
-        // Authorize a client with credentials, then call the Google Drive API.
-        const spinner = ora().start('Downloading file');
-        authorize(JSON.parse(content), downloadFile);
-        // console.log('File Downloaded')
-        spinner.stop();
-        spinner.succeed('File Downloaded');
-    });
-
-    function downloadFile(auth) {
-        const drive = google.drive({
-            version: 'v3',
-            auth
-        });
-
-        let fileId = el.attachments.match(/[-\w]{25,}/);
-        let dest = fs.createWriteStream(ABSPATH + '/attachments/document_' + i + '.pdf');
-
-        const spinner = ora().start('Sending email');
-
-        drive.files.get({
-            fileId: fileId,
-            alt: 'media'
-        }, {
-            responseType: 'stream'
-        }, (err, res) => {
-            res.data
-                .on('end', () => {
-                    const options = {
-                        from: 'Ray',
-                        to: el.mail,
-                        cc: 'rayrinaldy@gmail.com',
-                        subject: 'Hello From Dropee',
-                        text: el.text,
-                        attachments: [{
-                            filename: 'Attachment.pdf',
-                            content: fs.createReadStream(ABSPATH + '/attachments/document_' + i + '.pdf')
-                        }]
-                    };
+csv()
+    .fromFile(csvFilePath)
+    .then((mailList)=>{
+        // If attachment folder is not exist, create one
+        if (!fs.existsSync(ABSPATH + '/attachments')) {
+            const spinner = ora().start('No folder found!');
+            fs.mkdirSync(ABSPATH + '/attachments');
+            spinner.succeed('Folder created');
+        }
+        
+        // Let the loop begin
+        mailList.forEach((el, i) => {
+            if(!el.attachments) { // Check if attachment exist or not
+                let mail = el.mail;
+                // let text = el.text;
+        
+                try {
+                    const spinner = ora().start('Sending email');
                     
-                    // Sending email
+                    let options = {
+                        from: '"Hakama Clothing" <hakamaclothing@gmail.com>',
+                        to: mail,
+                        subject: 'Halo dari Hakama!',
+                        // text: text,
+                        html: htmlTemplate
+                    };
+        
                     transporter.sendMail(options, (error, info) => {
                         if (error) {
-                            console.log(error, 'XXXXXXX');
+                            console.log(error);
                             spinner.fail('Failed to send email!');
-                            spinner.stop();
                         } else {
                             spinner.succeed('Email sent successfully to ' + info.envelope.to[0]);
-                            spinner.stop();
                         }
                     });
-                })
-                .on('error', err => {
-                    console.log('Error', err);
-                })
-                .pipe(dest);
+                } catch (error) {
+                    console.log(error)            
+                }
+            } else {
+                fs.readFile('credentials.json', (err, content) => {
+                    if (err) return console.log('Error loading client secret file:', err);
+            
+                    const spinner = ora().start('Downloading file');
+            
+                    // Authorize a client with credentials, then call the Google Drive API.
+                    authorize(JSON.parse(content), downloadFile);
+            
+                    spinner.stop();
+                    spinner.succeed('File Downloaded');
+                });
+            
+                function downloadFile(auth) {
+                    const drive = google.drive({
+                        version: 'v3',
+                        auth
+                    });
+            
+                    let fileId = el.attachments.match(/[-\w]{25,}/);
+                    let dest = fs.createWriteStream(ABSPATH + '/attachments/document_' + i + '.pdf');
+            
+                    drive.files.get({
+                        fileId: fileId,
+                        alt: 'media'
+                    }, {
+                        responseType: 'stream'
+                    }, (err, res) => {
+                        res.data
+                            .on('end', () => {
+                                let mail = el.mail;
+                                let text = el.text;
+                                let attachment = fs.createReadStream(ABSPATH + '/attachments/document_' + i + '.pdf');
+            
+                                try {
+                                    sendEmail(mail, text, attachment);
+                                } catch (error) {
+                                    console.log(error);
+                                }
+                            })
+                            .on('error', err => {
+                                console.log('Error', err);
+                            })
+                            .pipe(dest);
+                    })
+                }
+            }
+        
         })
-    }
-})
+
+        const sendEmail = (to, text, attachment) => {
+            const spinner = ora().start('Sending email');
+                    
+            let options = {
+                from: '"Dropee" <ray@dropee.com>',
+                to: to,
+                cc: 'rayrinaldy@gmail.com',
+                subject: 'Hello From Dropee',
+                text: text,
+                attachments: [{
+                    filename: 'Attachment.pdf',
+                    content: attachment
+                }]
+            };
+            
+            // Sending email
+            transporter.sendMail(options, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    spinner.fail('Failed to send email!');
+                } else {
+                    spinner.succeed('Email sent successfully to ' + info.envelope.to[0]);
+                }
+            });
+        }
+    })
